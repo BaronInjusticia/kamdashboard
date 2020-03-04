404662508d737c546eef44d0161f287733dc942d
Baron <antonio.quetzatlcoatl@gmail.com>
2020-03-04 09:40:45 -0600

    martes blitz

---
+++ src/views/DashboardKam/DashboardKam.js
@@ -0,0 +1,368 @@
+import React from 'react';
+
+//*CSS
+import { makeStyles } from '@material-ui/styles';
+import Grid from '@material-ui/core/Grid';
+
+import {
+  //*Top
+  TablaGeneral,
+  GraficaBasica,
+  //*Cards1
+  EquiposSinGarantia,
+  CapacidadValidacionGarantias,
+  CaracteristicasEnlaces,
+  AnalisisContratosCarriers,
+  //*Cards2
+  ManejoGoogleSheetsDocs,
+  UsoOnepassword,
+  ConocimientoFacturacion,
+  AnalisisNumericos,
+  //*Cards3
+  DisenoReportes,
+  VisionMisionValores,
+  OrotografiaRedaccion,
+  UsoHub,
+  //*Cards4
+  EstructuracionManuales,
+  ClientesTmaaS,
+  AltasBajasMoviles,
+  AltasBajasFijos,
+  //*Cards5
+  UsoAsana,
+  UsoDrive,
+  CompraCotizacionEquipos,
+  CompartirInformacion,
+  //*Cards6
+  EnvioEquipos,
+  ObtenerAltaProvedores,
+  ManejoProvedoresContactos,
+  ImpuestosServiciosTele,
+  //*Cards7
+  UsoExcel,
+  UsoPortalFacturacion,
+  ClientesPrincipalesContactos,
+  UsoZendesk,
+  //*Card8
+  Facturacion4PT
+} from './components';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    padding: theme.spacing(4)
+  }
+}));
+
+const DashboardKam = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Grid
+        container
+        spacing={4}
+      >
+        <Grid
+          item
+          lg={8}
+          md={12}
+          xl={9}
+          xs={12}
+        >
+          <TablaGeneral />
+        </Grid>
+        <Grid
+          item
+          lg={4}
+          md={6}
+          xl={3}
+          xs={12}
+        >
+          <GraficaBasica /> 
+        </Grid>
+        <Grid
+          item
+          lg={8}
+          md={12}
+          xl={9}
+          xs={12}
+        >
+          <TablaGeneral />
+        </Grid>
+        <Grid
+          item
+          lg={4}
+          md={6}
+          xl={3}
+          xs={12}
+        >
+          <GraficaBasica /> 
+        </Grid>
+        <Grid
+          item
+          lg={3}
+          sm={6}
+          xl={3}
+          xs={12}
+        >
+          <EquiposSinGarantia />
+        </Grid>
+        <Grid
+          item
+          lg={3}
+          sm={6}
+          xl={3}
+          xs={12}
+        >
+          <CapacidadValidacionGarantias />
+        </Grid>
+        <Grid
+          item
+          lg={3}
+          sm={6}
+          xl={3}
+          xs={12}
+        >
+          <CaracteristicasEnlaces/>
+        </Grid>
+        <Grid
+          item
+          lg={3}
+          sm={6}
+          xl={3}
+          xs={12}
+        >
+          <AnalisisContratosCarriers />
+        </Grid>
+        <Grid
+          item
+          lg={3}
+          sm={6}
+          xl={3}
+          xs={12}
+        >
+          <ManejoGoogleSheetsDocs />
+        </Grid>
+        <Grid
+          item
+          lg={3}
+          sm={6}
+          xl={3}
+          xs={12}
+        >
+          <UsoOnepassword />
+        </Grid>
+        <Grid
+          item
+          lg={3}
+          sm={6}
+          xl={3}
+          xs={12}
+        >
+          <ConocimientoFacturacion />
+        </Grid>
+        <Grid
+          item
+          lg={3}
+          sm={6}
+          xl={3}
+          xs={12}
+        >
+          <AnalisisNumericos />
+        </Grid>
+        <Grid
+          item
+          lg={3}
+          sm={6}
+          xl={3}
+          xs={12}
+        >
+          <DisenoReportes />
+        </Grid>
+        <Grid
+          item
+          lg={3}
+          sm={6}
+          xl={3}
+          xs={12}
+        >
+          <VisionMisionValores />
+        </Grid>
+        <Grid
+          item
+          lg={3}
+          sm={6}
+          xl={3}
+          xs={12}
+        >
+          <OrotografiaRedaccion />
+        </Grid>
+        <Grid
+          item
+          lg={3}
+          sm={6}
+          xl={3}
+          xs={12}
+        >
+          <UsoHub />
+        </Grid>
+        <Grid
+          item
+          lg={3}
+          sm={6}
+          xl={3}
+          xs={12}
+        >
+          <EstructuracionManuales />
+        </Grid>
+        <Grid
+          item
+          lg={3}
+          sm={6}
+          xl={3}
+          xs={12}
+        >
+          <ClientesTmaaS />
+        </Grid>
+        <Grid
+          item
+          lg={3}
+          sm={6}
+          xl={3}
+          xs={12}
+        >
+          <AltasBajasMoviles />
+        </Grid>
+        <Grid
+          item
+          lg={3}
+          sm={6}
+          xl={3}
+          xs={12}
+        >
+          <AltasBajasFijos />
+        </Grid>
+        <Grid
+          item
+          lg={3}
+          sm={6}
+          xl={3}
+          xs={12}
+        >
+          <UsoAsana />
+        </Grid>
+        <Grid
+          item
+          lg={3}
+          sm={6}
+          xl={3}
+          xs={12}
+        >
+          <UsoDrive />
+        </Grid>
+        <Grid
+          item
+          lg={3}
+          sm={6}
+          xl={3}
+          xs={12}
+        >
+          <CompraCotizacionEquipos />
+        </Grid>
+        <Grid
+          item
+          lg={3}
+          sm={6}
+          xl={3}
+          xs={12}
+        >
+          <CompartirInformacion />
+        </Grid>
+        <Grid
+          item
+          lg={3}
+          sm={6}
+          xl={3}
+          xs={12}
+        >
+          <EnvioEquipos />
+        </Grid>
+        <Grid
+          item
+          lg={3}
+          sm={6}
+          xl={3}
+          xs={12}
+        >
+          <ObtenerAltaProvedores />
+        </Grid>
+        <Grid
+          item
+          lg={3}
+          sm={6}
+          xl={3}
+          xs={12}
+        >
+          <ManejoProvedoresContactos />
+        </Grid>
+        <Grid
+          item
+          lg={3}
+          sm={6}
+          xl={3}
+          xs={12}
+        >
+          <ImpuestosServiciosTele />
+        </Grid>
+        <Grid
+          item
+          lg={3}
+          sm={6}
+          xl={3}
+          xs={12}
+        >
+          <UsoExcel />
+        </Grid>
+        <Grid
+          item
+          lg={3}
+          sm={6}
+          xl={3}
+          xs={12}
+        >
+          <UsoPortalFacturacion />
+        </Grid>
+        <Grid
+          item
+          lg={3}
+          sm={6}
+          xl={3}
+          xs={12}
+        >
+          <ClientesPrincipalesContactos />
+        </Grid>
+        <Grid
+          item
+          lg={3}
+          sm={6}
+          xl={3}
+          xs={12}
+        >
+          <UsoZendesk />
+        </Grid>
+        <Grid
+          item
+          lg={3}
+          sm={6}
+          xl={3}
+          xs={12}
+        >
+          <Facturacion4PT />
+        </Grid>
+        
+      </Grid>
+    </div>
+  );
+};
+
+export default DashboardKam;
